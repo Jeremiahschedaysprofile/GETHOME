a8c73e51cde2086d29e5ba9e8bbc82d0d1e4899e
Jeremiahschedaysprofile <jeremiah.scheday@hgs.hiddengeniusproject.org>
2023-07-30 15:10:33 -0400

    Update 2
    finished map
    added gamer over screen
    added healthbar

--- FINAL PROJECT (2)/Assets/Script/Health.cs
+++ FINAL PROJECT (2)/Assets/Script/Health.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth = 200;
 
+    public HealthBar healthBar;
+
+    private bool isDead;
+
+    public GameManagerScript gameManager;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-   public void TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        if(currentHealth <= 0)
+
+        healthBar.setHealth(currentHealth);
+        if(currentHealth <= 0 && !isDead)
         {
-            Destroy(gameObject);
+            isDead = true;
+            gameManager.gameOver();
+            Debug.Log("Dead");
         }
     }
 }
--- FINAL PROJECT (2)/Assets/Script/PlayerController.cs
+++ FINAL PROJECT (2)/Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,11 +9,26 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed;
     public float horizontalInput;
     public float forwardInput;
+    public int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
 
+        SetcountText();
+        winTextObject.SetActive(false);
+    }
+
+    void SetcountText()
+    {
+        countText.text = "Count: " + ToString();
+        if (count >= 14)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
@@ -24,5 +40,17 @@ public class PlayerController : MonoBehaviour
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 15;
+
+            SetcountText();
+        }
+        
     }
 }
